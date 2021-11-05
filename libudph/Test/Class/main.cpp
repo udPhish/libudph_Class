8148d3a592e7c191abaf390c5f85447658b3f3f2
udPhish <28509694+udPhish@users.noreply.github.com>
2021-11-05 14:15:31 -0400

    Update main.cpp

--- libudph/Test/Class/main.cpp
+++ libudph/Test/Class/main.cpp
@@ -1,1 +1,29 @@
-int main(){}
+#include <iostream>
+
+#include <libudph/Class/Event.h>
+int main()
+{
+  UD::Event::Queue<int> q;
+  UD::Event::Event<int> e1;
+  UD::Event::Event<int> e2;
+  q(e1);
+  q(e2);
+
+  UD::Event::Handler<int> h1(
+      [](int i)
+      {
+        std::cout << i;
+      });
+  UD::Event::Handler<int> h2(
+      [](int i)
+      {
+        std::cout << i;
+      });
+
+  h1(e1);
+  h1(e2);
+  h2(e1);
+
+  e1(23);
+  e2(2);
+}
